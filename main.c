a295b414d9c2576adc1821db1e1a060b3c8aefb9
francois <franzdiaz460@protonmail.com>
2025-11-05 11:37:22 +0400

    ref: recv from udp datagram

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +8,12 @@
 int main(int argc, char *argv[]) {
   struct sockaddr_in server;
 
-  int s_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  const int s_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (s_fd < 0) {
+    perror("[SOCKET ERROR]:");
+    exit(1);
+  }
 
   memset(&server, 0, sizeof(struct sockaddr));
 
@@ -22,21 +26,18 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  if (listen(s_fd, 1) < 0) {
-    perror("[LIS ERROR]:");
-    exit(1);
-  }
-  printf("[INFO]: Listening from port:6969\n");
-  struct sockaddr_storage storage;
-  socklen_t stor_sze = sizeof storage;
-  int c_sd = 0;
-  while (c_sd >= 0) {
-    printf("[INFO]: Accepting connections\n");
-    c_sd = accept(s_fd, (struct sockaddr *)&storage, &stor_sze);
-  }
 
-  if (s_fd < 0) {
-    perror("[SOCKET ERROR]:");
-    exit(1);
+  char client_buf[1024];
+  printf("[INFO]: Listening from port:6969\n");
+  while (true) {
+    uint32_t bytes_read = recvfrom(s_fd, client_buf,1024,0,nullptr,nullptr);
+    printf("[TEST]: bytes read =%d", bytes_read);
+    if(bytes_read == -1)
+    {
+      perror("[RECV FROM ERR]");
+      break;
+    }
+    break;
   }
+  printf("[TEST]: text from client =%s",client_buf);
 };
