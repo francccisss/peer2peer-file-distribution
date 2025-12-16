a43d9e8916ac12566164ddc9244ff25920c5689d
francois <franzdiaz460@protonmail.com>
2025-12-16 11:36:44 +0400

    ref: parsing json file

--- test/client_test.c
+++ test/client_test.c
@@ -1,9 +1,9 @@
-
 #include <arpa/inet.h>
 #include <asm-generic/errno-base.h>
 #include <bits/types/struct_timeval.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,45 +18,31 @@
 
 typedef struct {
   char file_hash[ID_SIZE];
-  char *date_created;
-  struct node_t **known_nodes;
-} file_info;
+  uint32_t piece_length;
+  uint64_t length;
+  char *name;
+  char *path;
+} desciptor_file;
 
 #define FILE_BUFFER_SIZE 256
 
+int parse_json_descriptor(char *descriptor_path, desciptor_file *file);
+
 int main(int argc, char **argv) {
   struct sockaddr_in src;
-  char buffer[FILE_BUFFER_SIZE];
-
   if (argc < 2) {
     printf("[ERROR] port not defined\n");
     return -1;
   }
-  char *file_path = argv[2];
+  char *descriptor_path = argv[2];
+  desciptor_file file = {0};
 
-  int df_file = open(file_path, O_RDONLY);
-  if (df_file < 0) {
-    fprintf(stderr, "[ERROR]: \n");
-    return df_file;
-  }
-  int read_bytes;
-  while ((read_bytes = read(df_file, buffer, FILE_BUFFER_SIZE)) > 0) {
-    if (read_bytes < 0) {
-      perror("[ERROR] reading from df_file\n");
-      return df_file;
-    }
-  };
-  printf("[TEST]: json data string = %s\n", buffer);
-
-  cJSON *df_json = cJSON_Parse(buffer);
-
-  if (df_json == NULL) {
-    fprintf(stderr, "[ERROR]: Unable to parse Json data\n");
+  if (parse_json_descriptor(descriptor_path, &file) < 0) {
+    fprintf(stderr, "[ERROR]: Unable to parse json descriptor\n");
     return 1;
-  }
+  };
 
   return 0;
-
   // // what is being distributed
   int host_port = atoi(argv[1]);
   printf("host_port=%d\n", host_port);
@@ -65,7 +51,6 @@ int main(int argc, char **argv) {
       .ip = "localhost",
       .port = host_port,
   };
-  file_info file = {.file_hash = "13", .date_created = "November 6 2025"};
   src.sin_family = AF_INET;
   src.sin_port = htons(node.port);
   src.sin_addr.s_addr = INADDR_ANY;
@@ -155,6 +140,88 @@ int main(int argc, char **argv) {
     recv_rpc(s_fd, &node, file.file_hash, &msg_buffer, neighboring_nodes,
              &wait);
   }
+
+  return 0;
+}
+
+int parse_json_descriptor(char *descriptor_path, desciptor_file *file) {
+
+  char buffer[FILE_BUFFER_SIZE];
+
+  int df_file = open(descriptor_path, O_RDONLY);
+  if (df_file < 0) {
+    fprintf(stderr, "[ERROR]: \n");
+    return df_file;
+  }
+  int read_bytes;
+  while ((read_bytes = read(df_file, buffer, FILE_BUFFER_SIZE)) > 0) {
+    if (read_bytes < 0) {
+      perror("[ERROR] reading from df_file\n");
+      return df_file;
+    }
+  };
+
+  cJSON *df_json = cJSON_Parse(buffer);
+
+  if (df_json == NULL) {
+    fprintf(stderr, "[ERROR]: Unable to parse Json data\n");
+    return 1;
+  }
+
+  // #TODO: get the array of peers and files
+  cJSON *f = cJSON_GetObjectItemCaseSensitive(df_json, "file");
+
+  if (f == NULL && !cJSON_IsObject(f)) {
+    fprintf(stderr, "[ERROR]: {file} key is Null or not Object\n");
+    return 1;
+  }
+
+  char *f_str = cJSON_Print(f);
+  printf("[TEST]: print file object =%s\n", f_str);
+  cJSON *f_path = cJSON_GetObjectItemCaseSensitive(f, "path");
+  if (!cJSON_IsString(f_path)) {
+    fprintf(stderr, "[ERROR]: {path} key is is not String\n");
+    return 1;
+  }
+
+  cJSON *f_name = cJSON_GetObjectItemCaseSensitive(f, "name");
+
+  if (!cJSON_IsString(f_name)) {
+    fprintf(stderr, "[ERROR]: {name} key is is not String\n");
+    return 1;
+  }
+
+  cJSON *f_len = cJSON_GetObjectItemCaseSensitive(f, "length");
+
+  if (!cJSON_IsNumber(f_len)) {
+    fprintf(stderr, "[ERROR]: {length} key is is not Number\n");
+    return 1;
+  }
+
+  file->path = f_path->valuestring;
+  file->name = f_name->valuestring;
+  file->length = f_len->valueint;
+
+  cJSON *f_hk = cJSON_GetObjectItemCaseSensitive(df_json, "hash_key");
+  if (f_hk == NULL && !cJSON_IsString(f_hk)) {
+    fprintf(stderr, "[ERROR]: {length} key is is not String\n");
+    return 1;
+  }
+
+  cJSON *f_pclen = cJSON_GetObjectItemCaseSensitive(df_json, "piece_length");
+  if (f_pclen == NULL && !cJSON_IsNumber(f_pclen)) {
+    fprintf(stderr, "[ERROR]: {length} key is is not Number\n");
+    return 1;
+  }
+
+  strcpy(file->file_hash, f_hk->valuestring);
+  file->piece_length = f_pclen->valueint;
+
+  printf("[TEST] path=%s\n", file->path);
+  printf("[TEST] name=%s\n", file->name);
+  printf("[TEST] length=%ld\n", file->length);
+  printf("[TEST] hash=%s\n", file->file_hash);
+  printf("[TEST] piece_length=%d\n", file->piece_length);
 
   return 0;
 }
